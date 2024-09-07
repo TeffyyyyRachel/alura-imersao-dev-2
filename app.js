0e9374ea3e438226c097d43fac4e18efb408f77f
Stéphanie Rachel <stephrachel12@gmail.com>
2024-09-07 15:50:09 -0300

    update

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let section = document.getElementById("conteudo-pesquisado");
-let botao = document.getElementById("botao-pesquisa")
+let botao = document.getElementById("botao-pesquisa");
 
 botao.addEventListener("click", () => {
     let resultados = "";
-
+    let barra = document.getElementById("barra-pesquisa").value;
+        
     for (let dado of mangas) {
     resultados += `
         <div class="conteudo">
@@ -11,21 +12,19 @@ botao.addEventListener("click", () => {
                 <img src="${dado.capa}" alt="${dado.alt}" class="capa-manga">
             </div>
             <div class="informacoes">
-                <h2 class="titulo">${dado.titulo}</h2>
+                <h2 class="titulo">${dado.titulo} <span class="publicacao">(${dado.publicacao})</span></h2>
                 <p class="sinopse">${dado.descricao}</p>
                 <div class="info-outro">
                     <div class="autoria">
-                        <div id="escritor"> Escritor: ${dado.escritor}</div>
+                        <div id="escritor"> Escrito por: ${dado.escritor}</div>
                         <div id="desenhista">Desenhista: ${dado.desenhista}</div>
                     </div>
-                    <a href="${dado.link}" class="link-infos">Link para mais informações</a>
+                    <a href="${dado.link}" class="link-infos" target="_blank">Link para mais informações</a>
                 </div>
             </div>
         </div>
-    `
-    console.log(dado);
-    
-}
+    `   
+    }
 
 section.innerHTML = resultados
 })
