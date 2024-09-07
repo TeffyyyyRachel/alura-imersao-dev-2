9f4b6ad3b9ca49efeaf349159f72af375e28a8e5
Stéphanie Rachel <stephrachel12@gmail.com>
2024-09-07 16:22:17 -0300

    sistema de pesquisa finalizado

--- app.js
+++ app.js
@@ -2,29 +2,51 @@ let section = document.getElementById("conteudo-pesquisado");
 let botao = document.getElementById("botao-pesquisa");
 
 botao.addEventListener("click", () => {
-    let resultados = "";
-    let barra = document.getElementById("barra-pesquisa").value;
+
+    let resultados = ""; // Vai receber o bloco com as informações dos dados organizadas
+    
+    let barraPesquisa = document
+        .getElementById("barra-pesquisa")
+        .value
+        .trim();
+    // Recebe o que foi pesquisado na barra de pesquisa
         
     for (let dado of mangas) {
-    resultados += `
-        <div class="conteudo">
-            <div class="imagem">
-                <img src="${dado.capa}" alt="${dado.alt}" class="capa-manga">
-            </div>
-            <div class="informacoes">
-                <h2 class="titulo">${dado.titulo} <span class="publicacao">(${dado.publicacao})</span></h2>
-                <p class="sinopse">${dado.descricao}</p>
-                <div class="info-outro">
-                    <div class="autoria">
-                        <div id="escritor"> Escrito por: ${dado.escritor}</div>
-                        <div id="desenhista">Desenhista: ${dado.desenhista}</div>
+
+        if (!barraPesquisa) { //Não mostra nada se a barraPesquisa estiver vazia
+            resultados = "<p class='nada-encontrado'>Nada foi encontrado</p>"
+        }
+
+        else if (dado.titulo.toLowerCase().includes(barraPesquisa) || 
+        dado.descricao.toLowerCase().includes(barraPesquisa) ||
+        dado.tags.toLowerCase().includes(barraPesquisa) ||
+        dado.escritor.toLowerCase().includes(barraPesquisa) ||
+        dado.desenhista.toLowerCase().includes(barraPesquisa)) { // Testando se o que foi pesquisado em barraPesquisa existe em algum dos títulos
+            
+            // Se existir, resultados organiza isso em uma div estruturada para ser mostrada
+            resultados += `
+            <div class="conteudo">
+                <div class="imagem">
+                    <img src="${dado.capa}" alt="${dado.alt}" class="capa-manga">
+                </div>
+                <div class="informacoes">
+                    <h2 class="titulo">${dado.titulo} <span class="publicacao">(${dado.publicacao})</span></h2>
+                    <p class="sinopse">${dado.descricao}</p>
+                    <div class="info-outro">
+                        <div class="autoria">
+                            <div id="escritor"> Escrito por: ${dado.escritor}</div>
+                            <div id="desenhista">Desenhista: ${dado.desenhista}</div>
+                        </div>
+                        <a href="${dado.link}" class="link-infos" target="_blank">Link para mais informações</a>
                     </div>
-                    <a href="${dado.link}" class="link-infos" target="_blank">Link para mais informações</a>
                 </div>
             </div>
-        </div>
-    `   
+            `
+
+        }
+
     }
 
-section.innerHTML = resultados
+    section.innerHTML = resultados
+
 })
--- dados.js
+++ dados.js
@@ -10,7 +10,8 @@ PADRÃO PARA CRIAR OBJETOS
     descricao: "",
     escritor: "",
     desenhista: "",
-    link: ""
+    link: "",
+    tags: ""
 },
 
 */
@@ -24,7 +25,8 @@ let mangas = [
         descricao: "No Japão da era Taisho, Tanjiro Kamado é um menino de bom coração que ganha a vida vendendo carvão. No entanto, sua vida pacífica é destruída quando um Oni mata sua família inteira. Sua irmã mais nova, Nezuko, é a única sobrevivente, mas ela mesma se transformou em um Oni! Tanjiro parte em uma jornada perigosa para encontrar uma maneira de recuperar a humanidade de sua irmã e destruir o Oni que arruinou sua vida.",
         escritor: "Koyoharu Gotouge",
         desenhista: "Koyoharu Gotouge",
-        link: "https://pt.wikipedia.org/wiki/Kimetsu_no_Yaiba"
+        link: "https://pt.wikipedia.org/wiki/Kimetsu_no_Yaiba",
+        tags: "demonio demônio onis oni hashira Tanjiro Nezuko Inosuke Zenitsu taishou"
     },
     {
         capa: "imagens/capas_manga/one_piece.jpg",
@@ -34,7 +36,8 @@ let mangas = [
         descricao: "A série foca em Monkey D. Luffy, um jovem que comeu uma fruta mágica chamada de <i>Akuma no Mi</i>, em específico uma do tipo <i>Gomu Gomu no Mi</i> (ou Fruta da Borracha), e ganhou o poder de se esticar como se não tivesse ossos. Resistente a balas e usando golpes criativos, ele parte em uma jornada pelo mar do East Blue para encontrar um tesouro lendário, o One Piece, que realizará o seu grande sonho de proclamar-se o Rei dos Piratas.",
         escritor: "Eiichiro Oda",
         desenhista: "Eiichiro Oda",
-        link: "https://pt.wikipedia.org/wiki/One_Piece"
+        link: "https://pt.wikipedia.org/wiki/One_Piece",
+        tags: "luffy nami sanji zoro onepiece tesouro pirata akumanomi gomugomunomi"
     },
     {
         capa: "imagens/capas_manga/jujutsu_kaisen.jpeg",
@@ -44,7 +47,8 @@ let mangas = [
         descricao: "Morando com seu avô, Yuji Itadori é um estudante do ensino médio que vive em uma Sendai atormentada por espíritos amaldiçoados, monstros criados dos sentimentos negativos da humanidade. Guiado pelo medo de ser morto por uma maldição que o atacava e aos seus amigos, ele engole um dedo amaldiçoado na esperança de ganhar poderes para protegê-los, tornando-se o novo anfitrião de Sukuna, uma outra maldição entitulada de <i>O rei das maldições</i>.",
         escritor: "Gege Akutami",
         desenhista: "Gege Akutami",
-        link: "https://pt.wikipedia.org/wiki/Jujutsu_Kaisen"
+        link: "https://pt.wikipedia.org/wiki/Jujutsu_Kaisen",
+        tags: "maldicoes maldições nobara yuji megumi satoru gojo gojou"
     },
     {
         capa: "imagens/capas_manga/death_note.jpg",
@@ -54,7 +58,8 @@ let mangas = [
         descricao: "Light Yagami é um estudante da cidade de Tóquio, no Japão. Um dia, sua vida sofre uma mudança radical ao encontra um estranho caderno chamado 'Death Note' com instruções sobre sua utilização: se escrevesse o nome de uma pessoa e visualizasse mentalmente o rosto desta, ela morreria. No início, Light desconfiava da autenticidade do caderno, mas, depois de testá-lo em duas ocasiões, ele percebe que seu poder era verdadeiro. Light, então, decide que o seu objetivo era matar todos os criminosos, a fim de purificar o mundo do mal e tornar-se o 'deus do novo mundo'.",
         escritor: "Tsugumi Ohba",
         desenhista: "Takeshi Obata",
-        link: "https://pt.wikipedia.org/wiki/Death_Note"
+        link: "https://pt.wikipedia.org/wiki/Death_Note",
+        tags: "l caderno"
     },  
     {
         capa: "imagens/capas_manga/shingeki_no_kyojin.jpg",
@@ -64,6 +69,7 @@ let mangas = [
         descricao: "Para se protegerem dos Titãs, seres enormes com a aparência humana e devoradores de pessoas, a humanidade construiu enormes muralhas de 50 metros para se abrigar. Houve cerca de um século de paz, até que um Titã de tamanho colossal abriu uma brecha na muralha com apenas um chute. Os Titãs, em seguida, invadem a cidade e fazem uma carnificina. No meio desse ataque, Eren assiste sua própria mãe sendo devorada por um Titã; traumatizado, o garoto decide se vingar e matar todos os Titãs da terra.",
         escritor: "Hajime Isayama",
         desenhista: "Hajime Isayama",
-        link: "https://pt.wikipedia.org/wiki/Shingeki_no_Kyojin"
+        link: "https://pt.wikipedia.org/wiki/Shingeki_no_Kyojin",
+        tags: "tita titas titãs gigantes Eren Mikasa Armin"
     },
 ];
