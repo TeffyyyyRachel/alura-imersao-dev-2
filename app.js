6f2abbefefc059c7e6ed27beda32b65b0e3288c3
Stéphanie Rachel <stephrachel12@gmail.com>
2024-09-06 23:20:18 -0300

    terminado template do main e conteúdos

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-let section = document.getElementById("resultados-pesquisa");
-let botao = document.getElementById("botao")
+let section = document.getElementById("conteudo-pesquisado");
+let botao = document.getElementById("botao-pesquisa")
 
 botao.addEventListener("click", () => {
     let resultados = "";
 
     for (let dado of mangas) {
     resultados += `
-        
+        <div class="conteudo">
+            <div class="imagem">
+                <img src="${dado.capa}" alt="${dado.alt}" class="capa-manga">
+            </div>
+            <div class="informacoes">
+                <h2 class="titulo">${dado.titulo}</h2>
+                <p class="sinopse">${dado.descricao}</p>
+                <div class="info-outro">
+                    <div class="autoria">
+                        <div id="escritor"> Escritor: ${dado.escritor}</div>
+                        <div id="desenhista">Desenhista: ${dado.desenhista}</div>
+                    </div>
+                    <a href="${dado.link}" class="link-infos">Link para mais informações</a>
+                </div>
+            </div>
+        </div>
     `
     console.log(dado);
     
--- dados.js
+++ dados.js
@@ -26,6 +26,7 @@ let mangas = [
     },
     {
         capa: "imagens/capas_manga/one_piece.jpg",
+        alt: "Capa do mangá de One Piece, octagésimo sétimo volume",
         titulo: "One Piece",
         descricao: "A série foca em Monkey D. Luffy, um jovem que comeu uma fruta mágica chamada de <i>Akuma no Mi</i>, em específico uma do tipo <i>Gomu Gomu no Mi</i> (ou Fruta da Borracha), e ganhou o poder de se esticar como se não tivesse ossos. Resistente a balas e usando golpes criativos, ele parte em uma jornada pelo mar do East Blue para encontrar um tesouro lendário, o One Piece, que realizará o seu grande sonho de proclamar-se o Rei dos Piratas.",
         escritor: "Eiichiro Oda",
@@ -34,6 +35,7 @@ let mangas = [
     },
     {
         capa: "imagens/capas_manga/jujutsu_kaisen.jpeg",
+        alt: "Capa do mangá de Jujutsu Kaisen, primeiro volume",
         titulo: "Jujutsu Kaisen",
         descricao: "Morando com seu avô, Yuji Itadori é um estudante do ensino médio que vive em uma Sendai atormentada por espíritos amaldiçoados, monstros criados dos sentimentos negativos da humanidade. Guiado pelo medo de ser morto por uma maldição que o atacava e aos seus amigos, ele engole um dedo amaldiçoado na esperança de ganhar poderes para protegê-los, tornando-se o novo anfitrião de Sukuna, uma outra maldição entitulada de <i>O rei das maldições</i>.",
         escritor: "Gege Akutami",
@@ -42,6 +44,7 @@ let mangas = [
     },
     {
         capa: "imagens/capas_manga/death_note.jpg",
+        alt: "Capa do mangá de Death Note, primeiro volume",
         titulo: "Death Note",
         descricao: "Light Yagami é um estudante da cidade de Tóquio, no Japão. Um dia, sua vida sofre uma mudança radical ao encontra um estranho caderno chamado 'Death Note' com instruções sobre sua utilização: se escrevesse o nome de uma pessoa e visualizasse mentalmente o rosto desta, ela morreria. No início, Light desconfiava da autenticidade do caderno, mas, depois de testá-lo em duas ocasiões, ele percebe que seu poder era verdadeiro. Light, então, decide que o seu objetivo era matar todos os criminosos, a fim de purificar o mundo do mal e tornar-se o 'deus do novo mundo'.",
         escritor: "Tsugumi Ohba",
